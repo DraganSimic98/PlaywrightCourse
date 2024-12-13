08991fd7111f02a087880c8905772de29a44ebc9
Dragan Simic <dragan.simic@htecgroup.com>
2024-12-13 22:26:49 +0100

    Update accoun tests

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,26 +1,42 @@
 import {test, expect} from '@playwright/test';
+import Account from '../pages/account.page';
 
 test.describe('My Account', () => {
+    let account : Account;
+
+    test.beforeEach(async ({ page }) => {
+        account = new Account(page);
+       await account.navigate();
+    })
+    
 
     test('Access Orders', async ({page})=> {
-        await page.goto('/my-account');
-        await page.locator('//*[@id="post-9"]/div/div/div/nav/ul/li[2]/a').click();
+    
+        await account.ordersBtn.click();
         await expect(page).toHaveURL(/.*orders/);
+
     });
     test('Access Downloads', async ({page}) => {
-        await page.goto('/my-account');
-        await page.locator('//*[@id="post-9"]/div/div/div/nav/ul/li[3]/a').click();
+  
+        await account.downloadBtn.click();
         await expect(page).toHaveURL(/.*downloads/);
+
     });
 });
 
 test.describe('Account Page', () => {
+    let account : Account;
     test.use({ storageState: 'notLoggedInState.json' });
 
+    test.beforeEach(async ({ page }) => {
+        account = new Account(page);
+        await account.navigate();
+    })
+
     test('Verify login and registration visible', async ({ page }) => {
-        await page.goto('/my-account');
-        await expect(page.locator('//div[@class="u-column1 col-1"]/h2[text() = "Login"]')).toBeVisible();
-        await expect(page.locator('//div[@class="u-column2 col-2"]/h2[text() = "Register"]')).toBeVisible();
+        
+        await expect(account.loginText).toBeVisible();
+        await expect(account.registerText).toBeVisible();
     }); 
 });
 
